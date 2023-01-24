4cba140e297d2ba14842b639299ef67fc3b791d6
Sanchir <emgushovs@mail.ru>
2023-01-24 03:43:02 +0300

    hi

---
+++ src/App.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import './scss/app.scss';
+
+import Header from './components/Header.jsx';
+import Categories from './components/Categories';
+import Sort from './components/Sort';
+import PizzaBlock from './components/PizzaBlock';
+
+function App() {
+	return (
+		<div className='App'>
+			<div className='wrapper'>
+				<Header />
+				<div className='content'>
+					<div className='container'>
+						<div className='content__top'>
+							<Categories />
+							<Sort />
+						</div>
+						<h2 className='content__title'>Все пиццы</h2>
+						<div className='content__items'>
+							<PizzaBlock price={100} />
+							<PizzaBlock price={2} />
+						</div>
+						<ul className='Pagination_root__uwB0O'>
+							<li className='previous disabled'>
+								<a
+									className=' '
+									tabIndex='-1'
+									role='button'
+									aria-disabled='true'
+									aria-label='Previous page'
+									rel='prev'
+								>
+									&lt;
+								</a>
+							</li>
+							<li className='selected'>
+								<a
+									rel='canonical'
+									role='button'
+									tabIndex='-1'
+									aria-label='Page 1 is your current page'
+									aria-current='page'
+								>
+									1
+								</a>
+							</li>
+							<li>
+								<a rel='next' role='button' tabIndex='0' aria-label='Page 2'>
+									2
+								</a>
+							</li>
+							<li>
+								<a role='button' tabIndex='0' aria-label='Page 3'>
+									3
+								</a>
+							</li>
+							<li className='next'>
+								<a
+									className=''
+									tabIndex='0'
+									role='button'
+									aria-disabled='false'
+									aria-label='Next page'
+									rel='next'
+								>
+									&gt;
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default App;
